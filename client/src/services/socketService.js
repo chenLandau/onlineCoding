eef531b02f468b8dd15561419e3e59f8008a4deb
Chen Landau <chen201296@gmail.com>
2023-12-03 04:55:21 +0200

    edit server

--- client/src/services/socketService.js
+++ client/src/services/socketService.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
-const socket = io("localhost:5000", {
+const URL = // "undefined" means the URL will be computed from the `window.location` object
+  process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
+
+const socket = io(URL, {
   autoConnect: false,
   transports: ["websocket"],
 });
--- server/server.js
+++ server/server.js
@@ -8,19 +8,26 @@ import cors from "cors";
 import { Server as SocketIOServer } from "socket.io";
 import { setUpIo } from "./services/socketService.js";
 import codingRoomRouter from "./routes/codingRoomRouter.js";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app); //creating http server
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cors());
-
 app.get("/", (req, res) => {
   res.send("server is running");
 });
@@ -28,6 +35,9 @@ app.use("/api/v1/codingRoom", codingRoomRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 const port = process.env.PORT || 5000;
 setUpIo(io);
